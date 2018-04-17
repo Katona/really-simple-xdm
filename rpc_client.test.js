9cd68a537dce16bd7af9e5533cdc6243df59d74a
Gabor Hornyak <ghornyak@chemaxon.com>
2018-04-17 11:36:37 +0200

    more test

--- rpc_client.test.js
+++ rpc_client.test.js
@@ -45,24 +45,41 @@ test('should register a response listener for callbacks', t => {
 	t.is(testBackend.onResponse.firstCall.args.length, 1);
 });
 
-test('should handle callback response.', t => {
+test('should handle callbacks.', t => {
 	const testBackend = {
 		sendMessage: sinon.stub(),
 		onResponse: sinon.stub()
 	}
 	const client = new Proxy({}, new RpcClientHandler(testBackend));
 	t.is(testBackend.onResponse.firstCall.args.length, 1);
+	// Grab the response listener which is registered on the messaging backend
 	const callbackResponseListener = testBackend.onResponse.firstCall.args[0];
 	const testCallback = sinon.stub();
+	// Register a callback
 	client.on('test', testCallback);
+	// Check registration message
 	t.is(testBackend.sendMessage.firstCall.args.length, 1);
-	const message = testBackend.sendMessage.firstCall.args[0];
-	t.is(message.type, 'CALLBACK_REGISTRATION');
-	t.is(message.args.length, 2);
-	t.is(message.args[0].type, 'string')
-	t.is(message.args[1].type, 'function')
-	const callbackArgument = message.args[1];
+	const cbRegistrationMessage = testBackend.sendMessage.firstCall.args[0];
+	t.is(cbRegistrationMessage.type, 'CALLBACK_REGISTRATION');
+	t.is(cbRegistrationMessage.args.length, 2);
+	t.is(cbRegistrationMessage.args[0].type, 'string')
+	t.is(cbRegistrationMessage.args[1].type, 'function')
+	const callbackArgument = cbRegistrationMessage.args[1];
+	// Trigger a callback response
 	callbackResponseListener({ type: 'CALLBACK', id: callbackArgument.id, args: ['firstArg', 'secondArg'] });
 	t.is(testCallback.callCount, 1);
 	t.deepEqual(testCallback.firstCall.args, ['firstArg', 'secondArg']);
+
+	// Deregister callback
+	client.off('test', testCallback);
+	// Check message
+	t.is(testBackend.sendMessage.callCount, 2);
+	const cbDeregistrationMessage = testBackend.sendMessage.secondCall.args[0];
+	t.is(cbDeregistrationMessage.type, 'CALLBACK_DEREGISTRATION');
+	t.is(cbDeregistrationMessage.args.length, 2);
+	t.is(cbDeregistrationMessage.args[0].type, 'string')
+	t.is(cbDeregistrationMessage.args[1].type, 'function')
+
+	callbackResponseListener({ type: 'CALLBACK', id: cbDeregistrationMessage.args[1].id, args: ['firstArg']});
+	t.is(testCallback.callCount, 1, 'Callback should have not been invoked since it has been deregistered.');
 });
