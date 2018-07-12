7c594101f5240d076562f0c49dd499e2c078db89
Gabor Hornyak <ghornyak@chemaxon.com>
2018-07-12 21:51:39 +0200

    Do not use jsdom for test

--- test/cross_window_messaging_service_test.js
+++ test/cross_window_messaging_service_test.js
@@ -1,20 +1,48 @@
 import test from "ava";
-import jsdom from "jsdom";
+import sinon from "sinon";
 import CrossWindowMessagingService from "../src/cross_window_messaging_service";
 
-const { JSDOM } = jsdom;
+test.beforeEach(t => {
+    const targetOrigin = "targetOrigin";
+    t.context.window = {
+        addEventListener: sinon.spy()
+    };
+    t.context.target = {
+        postMessage: sinon.spy()
+    };
+    t.context.msgService = new CrossWindowMessagingService(t.context.target, targetOrigin, t.context.window);
+});
 
-test.cb("Test send message", t => {
-    const { window } = new JSDOM("", {
-        url: "http://bar.com/"
-    });
+test("Test send message", t => {
+    const testMessage = {
+        test: "test"
+    };
+    t.context.msgService.sendMessage({ test: "test" });
+    t.is(t.context.target.postMessage.callCount, 1);
+    t.deepEqual(t.context.target.postMessage.firstCall.args[0], testMessage);
+    t.is(t.context.target.postMessage.firstCall.args[1], "targetOrigin");
+});
 
-    var frame = window.document.createElement("iframe");
-    window.document.body.appendChild(frame);
-    frame.contentWindow.addEventListener("message", e => {
-        t.is(e.data.message, "hello");
-        t.end();
-    });
-    const msgService = new CrossWindowMessagingService(frame.contentWindow, "http://bar.com", window);
-    msgService.sendMessage({ message: "hello" });
+test("Test receive message", t => {
+    const messageListener = sinon.spy();
+    t.context.msgService.onMessage(messageListener);
+    t.is(t.context.window.addEventListener.callCount, 1);
+    t.is(t.context.window.addEventListener.firstCall.args[0], "message");
+    const eventListener = t.context.window.addEventListener.firstCall.args[1];
+
+    const messageFromOtherOrigin = {
+        origin: "otherOrigin"
+    };
+    eventListener(messageFromOtherOrigin);
+    t.is(messageListener.callCount, 0);
+
+    const messageFromExpectedOrigin = {
+        origin: "targetOrigin",
+        data: {
+            test: "test"
+        }
+    };
+    eventListener(messageFromExpectedOrigin);
+    t.is(messageListener.callCount, 1);
+    t.is(messageListener.firstCall.args[0], messageFromExpectedOrigin.data);
 });
