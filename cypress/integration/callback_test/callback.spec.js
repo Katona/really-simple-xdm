9a29ddceb60a29d909a3db1b49eabd999a9f4264
Gabor Hornyak <ghornyak@chemaxon.com>
2018-10-08 20:09:10 +0200

    Better integration test for callbacks

--- cypress/integration/callback_test/callback.spec.js
+++ cypress/integration/callback_test/callback.spec.js
@@ -1,6 +1,23 @@
 describe("Messages", () => {
+    function clickOnTestEventButton(cy) {
+        return cy.get("#testFrame").then(function($iframe) {
+            const doc = $iframe.contents();
+            const button = doc.find("#test-event");
+            cy.wrap(button).click();
+        });
+    }
+
     it("should appear on load", () => {
         cy.visit("http://localhost:8080/callback_test");
-        cy.get("#result").contains("Timeout! TestParam");
+        cy.wait(1000); // waiting for the iframe to load
+        clickOnTestEventButton(cy)
+            .then(() => {
+                cy.get("#result").contains("On click: 0");
+            })
+            .get("#stop-listening")
+            .click();
+        clickOnTestEventButton(cy).then(() => {
+            cy.get("#result").contains("On click: 0");
+        });
     });
 });
