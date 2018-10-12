ea5b3a17ee5a2c9fc8d278a172e6d3b777c73871
Gabor Hornyak <ghornyak@chemaxon.com>
2018-10-12 17:28:25 +0200

    Improve test messages

--- cypress/integration/callback_test/callback.spec.js
+++ cypress/integration/callback_test/callback.spec.js
@@ -1,5 +1,5 @@
-describe("Messages", () => {
-    it("should appear on load", () => {
+describe("Counter", () => {
+    it("should count up.", () => {
         // This test definitely needs improvement
         cy.visit("http://localhost:8080/callback_test");
         cy.get("#result").contains("2");
--- cypress/integration/event_listener_test/event_listener.spec.js
+++ cypress/integration/event_listener_test/event_listener.spec.js
@@ -1,4 +1,4 @@
-describe("Messages", () => {
+describe("Event listeners", () => {
     function clickOnTestEventButton(cy) {
         return cy.get("#testFrame").then(function($iframe) {
             const doc = $iframe.contents();
@@ -7,7 +7,7 @@ describe("Messages", () => {
         });
     }
 
-    it("should appear on load", () => {
+    it("should work", () => {
         cy.visit("http://localhost:8080/event_listener_test");
         cy.wait(1000); // waiting for the iframe to load
         clickOnTestEventButton(cy)
